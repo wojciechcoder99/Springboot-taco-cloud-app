506379ea2f2ddf254db2a4ff0a8ef865d040f65c
wojo321 <wojtek.f.99@gmail.com>
2021-05-02 00:08:09 +0200

    Added get all tacos method

--- src/main/java/tacos/controllers/DesignTacoController.java
+++ src/main/java/tacos/controllers/DesignTacoController.java
@@ -9,6 +9,8 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -42,7 +44,10 @@ public class DesignTacoController {
 		this.iTacoService = iTacoService;
 	}
 	
-	
+	@GetMapping
+	public @ResponseBody ResponseEntity<Iterable<Taco>> getTacos(){
+		return new ResponseEntity<Iterable<Taco>>(iTacoService.getTacos(), HttpStatus.OK);
+	}
 
 	 
 }
